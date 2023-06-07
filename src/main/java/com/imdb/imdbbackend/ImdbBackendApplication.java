6c92899cc502542ddeaddaaa615ee5515af1d2ea
PAYAM SOUDACHI <payams@energizeglobal.com>
2023-06-07 11:52:48 +0400

    Add spring boot application class to the project.

---
+++ src/main/java/com/imdb/imdbbackend/ImdbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imdb.imdbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbBackendApplication.class, args);
+	}
+
+}
